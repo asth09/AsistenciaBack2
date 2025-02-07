7274d991356f11cb8a76ebc869751ce9f216119d
s3uz-dev <suzzenelson@gmail.com>
2025-02-07 15:13:19 -0300

    cambio url cors 3

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ const corsOptions = {
 app.use(cors(corsOptions)); 
 app.use(cookieParser());
 app.use(bodyParser.json())
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/api/nelson", (req, res) =>{
   res.json({
