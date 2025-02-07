ec80c0b88ed47bd2e8447a9e648887b6d8fcfcfb
asth09 <asthackerman@gmail.com>
2025-02-07 15:52:16 -0400

    base de datos

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://estebanquito409:XJaBWgMKpFy9ktZM@cluster0.xbkovdh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        await mongoose.connect('mongodb+srv://asdrithcontreras:asdrit+-*@cluster0.pvu1i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         console.log("Base de datos conectada")
     } catch (error) {
         console.log(error)
